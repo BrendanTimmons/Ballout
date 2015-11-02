533f305cb056264260447a1223b537b8c7318977
BrendanTimmons <brendantimmons@live.com.au>
2015-11-03 09:17:42 +1100

    changed collisions to the arcade physics instead of manual

--- js/game.js
+++ js/game.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render });
 
-  function preload() {
+  function preload(){
     game.load.image('sky', 'assets/sky.png');
     game.load.image('ground', 'assets/platform.png');
     game.load.image('star', 'assets/star.png');
@@ -9,9 +9,10 @@ $(document).ready(function(){
 
   var blocks,
       walls,
+      roof,
       ball = {
-        velX: -3,
-        velY: 3
+        velX: -100,
+        velY: 100
       },
       player = {
         lives: 3
@@ -20,12 +21,13 @@ $(document).ready(function(){
       paddleSpeed = 4,
       livesText;
 
-  function create() {
+  function create(){
     cursors = game.input.keyboard.createCursorKeys();
+    game.physics.startSystem(Phaser.Physics.ARCADE);
 
     game.add.sprite(0, 0, 'sky');
 
-    walls = game.add.group();
+    walls = game.add.physicsGroup();
     walls.enableBody = true;
 
     var wall = walls.create(0, 0, 'ground');
@@ -36,9 +38,10 @@ $(document).ready(function(){
     wall.scale.setTo(0.05, 60);
     wall.body.immovable = true;
 
-    wall = walls.create(0, -10, 'ground');
-    wall.scale.setTo(60, 1);
-    wall.body.immovable = true;
+    roof = game.add.sprite(0, -10, 'ground');
+    roof.scale.setTo(60, 1);
+    game.physics.enable(roof, Phaser.Physics.ARCADE);
+    roof.body.immovable = true;
 
 //    blocks = game.add.group();
 //    blocks.enableBody = true;
@@ -49,31 +52,35 @@ $(document).ready(function(){
 
     ball.sprite = game.add.sprite(350, game.world.height - 130, 'star');
     game.physics.enable(ball.sprite, Phaser.Physics.ARCADE);
+    ball.sprite.body.collideWorldBounds = true;
 
     player.sprite = game.add.sprite(300, game.world.height - 70, 'dank');
-    game.physics.arcade.enable(player);
     player.sprite.scale.setTo(2, 0.5);
+    game.physics.enable(player.sprite, Phaser.Physics.ARCADE);
+    player.sprite.body.immovable = true;
 
     livesText = game.add.text(30, game.world.height -30, 'Lives: ' + player.lives, {fontSize: '20px', fill: 'red'});
+
   }
 
-  function update() {
+  function update(){
     movePlayer();
-    wallCollision();
-    ballPlayerCollision();
     livesText.text = 'Lives: ' + player.lives;
-    game.physics.arcade.collide(ball.sprite, player.sprite, ass, null, this);
 
-    ball.sprite.y += ball.velY;
-    ball.sprite.x += ball.velX;
+    ball.sprite.body.velocity.x = ball.velX;
+    ball.sprite.body.velocity.y = ball.velY;
+
+    game.physics.arcade.collide(ball.sprite, walls, wallCollision, null, this);
+    game.physics.arcade.collide(ball.sprite, roof, roofCollision, null, this);
+    game.physics.arcade.collide(ball.sprite, player.sprite, ballHit, null, this);
   }
 
-  function render() {
+  function render(){
     game.debug.body(ball.sprite);
   }
 
-  function ass() {
-    console.log("collided");
+  function collisionHandler(){
+    console.log("ass");
   }
 
   function movePlayer(){
@@ -93,25 +100,14 @@ $(document).ready(function(){
   }
 
   function wallCollision(){
-    if (ball.sprite.x < 0){
-      ball.velX = -ball.velX;
-    } else if (ball.sprite.y < 0){
-      ball.velY = -ball.velY;
-    } else if (ball.sprite.x > game.world.width - ball.sprite.width){
-      ball.velX = -ball.velX;
-    } else if (ball.sprite.y > game.world.height - ball.sprite.height){
-      ball.velY = -ball.velY;
-      player.lives -= 1;
-    }
+    ball.velX = -ball.velX;
   }
 
-  function ballPlayerCollision(){
-    if ((ball.sprite.y + ball.sprite.height) >= player.sprite.y
-                && ball.sprite.x >= player.sprite.x 
-                && ball.sprite.x <= (player.sprite.x + player.sprite.width)
-                && ball.velY > 0
-                && (ball.sprite.y + ball.sprite.height) < (player.sprite.y + player.sprite.height)){
-      ball.velY = -ball.velY;
-    }
+  function roofCollision(){
+    ball.velY = -ball.velY;
+  }
+
+  function ballHit(){
+    ball.velY = -ball.velY;
   }
 });
