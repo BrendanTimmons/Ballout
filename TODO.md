d11028d39d8961825305c39340a403876190df56
BrendanTimmons <brendantimmons@live.com.au>
2015-11-03 10:42:23 +1100

    got the game over stuff half done

--- TODO.md
+++ TODO.md
@@ -1,3 +1,4 @@
 * Split out roof and walls into separate groups.
 * Finish doing the death logic
   * ASSSS 
+* Full pause functionality, currently can still move player when paused.
--- js/game.js
+++ js/game.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render });
+  var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update});
 
   function preload(){
     game.load.image('sky', 'assets/sky.png');
@@ -19,7 +19,8 @@ $(document).ready(function(){
       },
       cursors,
       paddleSpeed = 4,
-      livesText;
+      livesText,
+      gameStateText;
 
   function create(){
     cursors = game.input.keyboard.createCursorKeys();
@@ -43,16 +44,15 @@ $(document).ready(function(){
     game.physics.enable(roof, Phaser.Physics.ARCADE);
     roof.body.immovable = true;
 
-//    blocks = game.add.group();
+//    blocks = game.add.physicsGroup();
 //    blocks.enableBody = true;
 
 //    for (var i = 1; i < 10; i++){
 //      var block = blocks.create(i * 70, 30, 'block');
 //    }
 
-    ball.sprite = game.add.sprite(350, game.world.height - 130, 'star');
+    ball.sprite = game.add.sprite(game.world.centerX, game.world.centerY, 'star');
     game.physics.enable(ball.sprite, Phaser.Physics.ARCADE);
-    ball.sprite.body.collideWorldBounds = true;
 
     player.sprite = game.add.sprite(300, game.world.height - 70, 'dank');
     player.sprite.scale.setTo(2, 0.5);
@@ -60,41 +60,51 @@ $(document).ready(function(){
     player.sprite.body.immovable = true;
 
     livesText = game.add.text(30, game.world.height -30, 'Lives: ' + player.lives, {fontSize: '20px', fill: 'red'});
+    gameStateText = game.add.text(game.world.centerX, game.world.centerY, '', {fontSize: '40px', fill: 'red'});
+    gameStateText.anchor.set(0.5);
 
+    pauseKey = this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+    pauseKey.onDown.add(togglePause, this);
   }
 
   function update(){
     movePlayer();
-    livesText.text = 'Lives: ' + player.lives;
+    updateHUD();
 
     ball.sprite.body.velocity.x = ball.velX;
     ball.sprite.body.velocity.y = ball.velY;
 
     game.physics.arcade.collide(ball.sprite, walls, wallCollision, null, this);
     game.physics.arcade.collide(ball.sprite, roof, roofCollision, null, this);
-    game.physics.arcade.collide(ball.sprite, player.sprite, ballHit, null, this);
+    game.physics.arcade.collide(ball.sprite, player.sprite, playerBallCollision, null, this);
+
+    outOfBounds();
   }
 
-  function render(){
-    game.debug.body(ball.sprite);
-  }
+  function updateHUD(){
+    livesText.text = 'Lives: ' + player.lives;
 
-  function collisionHandler(){
-    console.log("ass");
+    if(player.lives == 0){
+      gameStateText.text = 'Game Over!';
+    }
   }
 
   function movePlayer(){
     if (cursors.left.isDown && player.sprite.x > game.world.bounds.left){
       if(cursors.left.shiftKey){
         player.sprite.x -= paddleSpeed * 2;
+        game.physics.arcade.isPaused = false;
       } else {
         player.sprite.x -= paddleSpeed;
+        game.physics.arcade.isPaused = false;
       }
     } else if (cursors.right.isDown && (player.sprite.x + player.sprite.width) < game.world.bounds.right){
       if(cursors.right.shiftKey){
         player.sprite.x += paddleSpeed * 2;
+        game.physics.arcade.isPaused = false;
       } else {
         player.sprite.x += paddleSpeed;
+        game.physics.arcade.isPaused = false;
       }
     }
   }
@@ -107,7 +117,19 @@ $(document).ready(function(){
     ball.velY = -ball.velY;
   }
 
-  function ballHit(){
+  function playerBallCollision(){
     ball.velY = -ball.velY;
+  }
+
+  function togglePause(){
+    game.physics.arcade.isPaused = (game.physics.arcade.isPaused) ? false : true;
+  }
+
+  function outOfBounds(){
+    if(ball.sprite.y > game.world.height){
+      player.lives -= 1;
+      ball.sprite.x = game.world.centerX;
+      ball.sprite.y = game.world.centerY;
+    }
   }
 });
