d980dce9ff0572f528f76e5413d76dcc84c53761
BrendanTimmons <brendantimmons@live.com.au>
2015-11-14 18:16:31 +1100

    made the menu text flash

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,10 @@
+var startText,
+    timer = 0;
+
 var menuState = {
   create: function(){
     game.add.sprite(0,0, 'splashbg');
-    var startText = game.add.text(game.world.centerX / 2, game.world.centerY + 50, "Press Enter to start", {fontSize: '40px', fill: '#ff5dbd'});
+    startText = game.add.text(game.world.centerX / 2, game.world.centerY + 50, "Press Enter to start", {fontSize: '40px', fill: '#ff5dbd'});
     startText.setShadow(-1, 1, 'rgba(0,0,0,1)', 0);
 
     var enterKey = game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
@@ -12,6 +15,16 @@ var menuState = {
     music = game.add.audio('soundtrackMenu');
     music.loop = true;
     music.play();
+  },
+
+  update: function(){
+    timer += game.time.elapsed;
+    if(timer >= 600){
+      console.log("ass");
+      timer -= 600;
+      startText.visible = !startText.visible;
+    }
+
   },
 
   start: function(){
