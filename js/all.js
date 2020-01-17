7e7fb726821758f2351eac594a5f465b5301cd12
Brendan Timmons <brendantimmons@live.com.au>
2020-01-18 00:15:54 +1100

    add log for velocity.

--- js/all.js
+++ js/all.js
@@ -487,6 +487,7 @@ var playState = {
   playerBallCollision: function () {
     var collisionLoc = (playState.ball.sprite.x + playState.ball.sprite.width / 2 - playState.player.sprite.x) / playState.player.sprite.width;
     var newVel = Math.sqrt(Math.pow(playState.ball.sprite.body.velocity.x, 15) + Math.pow(playState.ball.sprite.body.velocity.y, 15));
+    console.log(newVel);
     game.physics.arcade.velocityFromAngle(225 + collisionLoc * 90, newVel, playState.ball.sprite.body.velocity);
     blip.play();
     playState.combo = 1;
