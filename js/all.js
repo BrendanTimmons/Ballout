404c4bd7aa4cf0692697c63c931f9df7cd53d6de
Brendan Timmons <brendantimmons@live.com.au>
2020-01-18 00:43:01 +1100

    revert velocity change.

--- js/all.js
+++ js/all.js
@@ -485,8 +485,9 @@ var playState = {
   },
 
   playerBallCollision: function () {
+    console.log(playState.ball.sprite.body.velocity)
     var collisionLoc = (playState.ball.sprite.x + playState.ball.sprite.width / 2 - playState.player.sprite.x) / playState.player.sprite.width;
-    var newVel = Math.sqrt(Math.pow(playState.ball.sprite.body.velocity.x, 2.5) + Math.pow(playState.ball.sprite.body.velocity.y, 2.5));
+    var newVel = Math.sqrt(Math.pow(playState.ball.sprite.body.velocity.x, 2) + Math.pow(playState.ball.sprite.body.velocity.y, 2));
     game.physics.arcade.velocityFromAngle(225 + (collisionLoc * 90), newVel, playState.ball.sprite.body.velocity);
     console.log(newVel);
     blip.play();
