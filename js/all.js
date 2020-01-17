5b98d71fd197c3d3aa2c445c6cc5996cde7e050b
Brendan Timmons <brendantimmons@live.com.au>
2020-01-18 00:10:19 +1100

    change collision velocity.

--- js/all.js
+++ js/all.js
@@ -486,7 +486,7 @@ var playState = {
 
   playerBallCollision: function () {
     var collisionLoc = (playState.ball.sprite.x + playState.ball.sprite.width / 2 - playState.player.sprite.x) / playState.player.sprite.width;
-    var newVel = Math.sqrt(Math.pow(playState.ball.sprite.body.velocity.x, 2) + Math.pow(playState.ball.sprite.body.velocity.y, 2));
+    var newVel = Math.sqrt(Math.pow(playState.ball.sprite.body.velocity.x, 4) + Math.pow(playState.ball.sprite.body.velocity.y, 4));
     game.physics.arcade.velocityFromAngle(225 + collisionLoc * 90, newVel, playState.ball.sprite.body.velocity);
     blip.play();
     playState.combo = 1;
